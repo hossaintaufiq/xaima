bb9242ea4a38ad525a6b3ae7a94d3c32ddc108e4
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-15 10:48:14 +0600

    first commit

---
+++ Scripts/event.js
@@ -0,0 +1,24 @@
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+
+}
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+
+}
+
+const makePurple =document.getElementById('Idk');
+makePurple.onclick= purple;
+
+function purple(){
+    document.body.style.backgroundColor ='purple';
+}
+
+
+const makePink = document.getElementById('who');
+
+makePink.onclick=pink;
+
+function pink(){
+    document.body.style.backgroundColor='pink';
+}
